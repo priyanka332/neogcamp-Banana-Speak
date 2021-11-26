d11c4b1ffdb41533885a9ae78dff911125ef91b0
Priyanka Kumari <priyanka20@navgurukul.org>
2021-11-26 15:17:19 +0530

    js update

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", getTranslationURL)
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverUrl + "?" + "text= " + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time.")
+}
 function clickHandler() {
-    outputDiv.innerText = "Puja Kumari " + txtInput.value;
+    var inputText = txtInput.value  // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translateText = json.contents.translated;
+            outputDiv.innerText = translateText
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
